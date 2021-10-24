5267d7de0d70a21584e6b197bc2c84d0f4a579d2
Luiz Henrique <7henrique18@gmail.com>
2021-10-24 18:43:30 -0300

    feat: component user photo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import LogoSvg from "../../assets/logo.svg";
+import { UserPhoto } from "../UserPhoto";
 
 import { styles } from "./styles";
 
 export function Header() {
   return (
-    <View>
+    <View style={styles.container}>
       <LogoSvg />
-      <Text>Sair</Text>
+      <UserPhoto imageUri="https://github.com/lui7henrique.png" />
+      <TouchableOpacity>
+        <Text style={styles.logoutText}>Sair</Text>
+      </TouchableOpacity>
     </View>
   );
 }
